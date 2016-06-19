2736bb0655044cad30c860894fea5b28683f2dd9
Sarkis M <melkonsa@gmail.com>
2016-06-19 09:41:22 -0700

    updated gitignore

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :facebook, '515550971961890', '0116e48f938a4862d7e269bc4fe12e84'
 end
